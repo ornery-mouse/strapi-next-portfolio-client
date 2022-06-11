40a2c44fc73c5a81ef69d50b34a73a0d788a6a4e
ornery-mouse <cory.sorel@gmail.com>
2022-06-11 16:01:48 -0500

    README updated and landing page made

---
+++ components/ProjectCard.js
@@ -0,0 +1,9 @@
+export default function ProjectCard({project}) {
+    return (
+        <div>
+            <div>
+                <h3>{project.nameB}</h3>
+            </div>
+        </div>
+    )
+}
